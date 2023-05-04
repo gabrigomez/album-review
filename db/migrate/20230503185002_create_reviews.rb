afd5ebf08a181a87a4ee1c062417ba57d89321b9
Gabriel <gabrigomez15@gmail.com>
2023-05-04 10:45:41 -0300

    fix: review relationship with albums

--- db/migrate/20230503185002_create_reviews.rb
+++ db/migrate/20230503185002_create_reviews.rb
@@ -4,7 +4,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
       t.string :title
       t.string :description
       t.integer :score
-      t.belongs_to :albums, null: false, foreign_key: true
+      t.belongs_to :album, null: false, foreign_key: true
 
       t.timestamps
     end
