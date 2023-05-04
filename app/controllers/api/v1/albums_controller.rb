f27be04a3f7136987f8ffec07a5d928ea4acdc8d
Gabriel <gabrigomez15@gmail.com>
2023-05-04 14:34:32 -0300

    feat: add albums_controller methods

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,6 +1,57 @@
 module Api
   module v1
     class AlbumsController < ApplicationController
+      def index
+        albums = Album.all
+
+        render json: AlbumSerializer.new(albums, options).serialized_json
+      end
+
+      def show
+        album = Album.find_by(slug: params[:slug])
+
+        render json: AlbumSerializer.new(album, opitons).serialized_json
+      end
+
+      def create
+        album = Album.new(album_params)
+        
+        if album.save
+          render json: AlbumSerializer.new(album).serialized_json
+        else
+          render json: { error: album.errors.messages }, status: 422          
+        end
+      end
+
+      def update
+        album = Album.find_by(slug: params[:slug])
+        
+        if album.update
+          render json: AlbumSerializer.new(album, options).serialized_json
+        else
+          render json: { error: album.errors.messages }, status: 422          
+        end
+      end
+      
+      def destroy
+        album = Album.find_by(slug: params[:slug])
+        
+        if album.destroy
+          head :no_content
+        else
+          render json: { error: album.errors.messages }, status: 422          
+        end
+      end 
+
+      private
+
+      def album_params
+        params.require(:album).permit(:name, :image_url)
+      end
+
+      def options
+        @options ||= { include: %i[reviews] } 
+      end      
     end
   end
 end
